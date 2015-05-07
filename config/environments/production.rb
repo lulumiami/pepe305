f544ad5d2b85a5a9c8facbc45ade39664f8127c9
MAL <marvinlusky@gmail.com>
2015-05-07 18:53:12 -0400

    updated refs to env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,8 @@ config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :access_key_id => ENV['AWSAccessKeyId'],
+    :secret_access_key => ENV['AWSSecretKey']
   }
 }
 
