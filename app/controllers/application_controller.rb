5c80a9168207049eb76de6eb01b4423424113e6d
Marilu Kernan <marilukernan@mac.com>
2015-05-07 15:27:19 -0400

    logout functionality added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+#   def after_sign_in_path_for(resource)
+#   	stored_location_for(resource) || 
+#   	if resource.is_a?(User &&) resource.
+
+def configure_permitted_parameters
+	[:sign_up, :account_update].each do |action|
+		devise_parameter_sanitizer.for(action).push(:admin)
+	end
+end	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507001917) do
+ActiveRecord::Schema.define(version: 20150507190424) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20150507001917) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
